124a1136e66021cf5821b52904a351dfa1f79323
Ben Puhalski <puhalskib@gmail.com>
2019-10-21 01:24:23 -0400

    testing reading and writing to files

--- app/src/main/java/com/example/loginactivity/MainActivity.kt
+++ app/src/main/java/com/example/loginactivity/MainActivity.kt
@@ -1,12 +1,54 @@
 package com.example.loginactivity
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.FileOutputStream
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loginButton.setOnClickListener {
+            //create intent
+            val intent = Intent(this, UserInstance::class.java)
+
+            //get username and password
+            val file:String = usernameField.text.toString()
+            val data:String = passwordField.text.toString()
+
+
+            val fileOutputStream:FileOutputStream
+            try {
+                fileOutputStream = openFileOutput(file, Context.MODE_PRIVATE)
+                fileOutputStream.write(data.toByteArray())
+            } catch (e: FileNotFoundException){
+                e.printStackTrace()
+            }catch (e: NumberFormatException){
+                e.printStackTrace()
+            }catch (e: IOException){
+                e.printStackTrace()
+            }catch (e: Exception){
+                e.printStackTrace()
+            }
+            Toast.makeText(applicationContext,"data save",Toast.LENGTH_LONG).show()
+
+
+
+
+            //pass data to intent
+            intent.putExtra("name1", file)
+
+            // start your next activity with intent
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/loginactivity/UserInstance.kt
+++ app/src/main/java/com/example/loginactivity/UserInstance.kt
@@ -1,9 +1,12 @@
 package com.example.loginactivity
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-
 import kotlinx.android.synthetic.main.activity_user_instance.*
+import java.io.BufferedReader
+import java.io.FileInputStream
+import java.io.InputStreamReader
 
 class UserInstance : AppCompatActivity() {
 
@@ -11,6 +14,31 @@ class UserInstance : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_instance)
 
+        //get intent
+        var strFile: String = intent.getStringExtra("name1")
+        //userData.text = strFile
+
+
+
+
+
+        val filename = strFile
+        if(filename.toString()!=null && filename.toString().trim()!=""){
+            var fileInputStream: FileInputStream? = null
+            fileInputStream = openFileInput(filename)
+            var inputStreamReader: InputStreamReader = InputStreamReader(fileInputStream)
+            val bufferedReader: BufferedReader = BufferedReader(inputStreamReader)
+            val stringBuilder: StringBuilder = StringBuilder()
+            var text: String? = null
+            while ({ text = bufferedReader.readLine(); text }() != null) {
+                stringBuilder.append(text)
+            }
+            //Displaying data on EditText
+            //fileData.setText(stringBuilder.toString()).toString()
+            userData.text = stringBuilder.toString()
+        } else {
+            Toast.makeText(applicationContext,"file name cannot be blank",Toast.LENGTH_LONG).show()
+        }
 
     }
 
