de4e4e80063d45e50940cfc35710cb695923d8ad
Ben Puhalski <puhalskib@gmail.com>
2019-10-20 22:52:52 -0400

    Changed userInstance Layout

---
+++ app/src/main/java/com/example/loginactivity/UserInstance.kt
@@ -0,0 +1,17 @@
+package com.example.loginactivity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_user_instance.*
+
+class UserInstance : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_instance)
+
+
+    }
+
+}
